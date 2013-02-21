e87fc5eb945c1f020c46f857f5090dadc75b50b7
David LaPalomento <dlapalomento@gmail.com>
2013-02-21 10:08:36 -0500

    Track mouse movement on hover Display the thumbnail and track mouse movements with the thumbnail while the user is hovering over the progress bar.

--- videojs.thumbnails.js
+++ videojs.thumbnails.js
@@ -27,8 +27,9 @@
    * register the thubmnails plugin
    */
   videojs.plugin('thumbnails', function(options) {
-    var settings, img, progressControl;
+    var settings, img, player, progressControl, duration, updateThumbnail;
     settings = extend(defaults, options);
+    player = this;
 
     // create the thumbnail image
     img = document.createElement('img');
@@ -36,8 +37,36 @@
     img.setAttribute('style', settings['0'].style || '');
     img.setAttribute('class', 'vjs-thumbnail');
 
+    // keep track of the duration to calculate correct thumbnail to display
+    duration = player.duration();
+    player.on('durationchange', function(event) {
+      duration = player.duration();
+    });
+
     // add the thumbnail to the player
-    progressControl = this.controlBar.progressControl.el();
-    progressControl.appendChild(img);
+    progressControl = player.controlBar.progressControl;
+    progressControl.el().appendChild(img);
+
+    // listener to adjust the thumbnail position while hovering
+    updateThumbnail = function(event) {
+      img.style.left = event.offsetX + 'px';
+    };
+    // display the thumbnail and listen for mouse movements
+    progressControl.on('mouseover', function(event) {
+      if (event.target === progressControl.el()) {
+        img.className += ' vjs-thumbnail-hover';
+        img.style.left = event.offsetX + 'px';
+        progressControl.el().addEventListener('mousemove', updateThumbnail, false);
+      }
+    });
+    // hide the thumbnail and stop listening for mouse movements
+    progressControl.on('mouseout', function(event) {
+      var toElement = event.relatedTarget || event.toElement;
+      if (toElement !== progressControl.seekBar.el()) {
+        img.className = img.className.replace(/(?:^|\s)vjs-thumbnail-hover(?!\S)/g , '');
+        progressControl.el().removeEventListener('mousemove', updateThumbnail);
+      }
+    });
+    
   });
 })();
