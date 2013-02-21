c014f3b288045b0d2826a6b9ea636782cc6c29f7
David LaPalomento <dlapalomento@gmail.com>
2013-02-21 08:57:46 -0500

    Thumbnail on Hover Show a thumbnail when the user hovers over the progress bar. It doesn't move yet or adjust itself to the mouse position.

--- videojs.thumbnails.js
+++ videojs.thumbnails.js
@@ -1,3 +1,43 @@
-videojs.plugin('thumbnails', function(options) {
-  var settings = options;
-});
+(function() {
+  var defaults = {
+      0: {
+        src: 'example-thumbnail.png'
+      }
+    },
+    extend = function() {
+      var args, target, i, object, property;
+      args = Array.prototype.slice.call(arguments);
+      target = args.shift() || {};
+      for (i in args) {
+        object = args[i];
+        for (property in object) {
+          if (object.hasOwnProperty(property)) {
+            if (typeof object[property] === 'object') {
+              target[property] = extend(target[property], object[property]);
+            } else {
+              target[property] = object[property];
+            }
+          }
+        }
+      }
+      return target;
+    };
+
+  /**
+   * register the thubmnails plugin
+   */
+  videojs.plugin('thumbnails', function(options) {
+    var settings, img, progressControl;
+    settings = extend(defaults, options);
+
+    // create the thumbnail image
+    img = document.createElement('img');
+    img.src = settings['0'].src;
+    img.setAttribute('style', settings['0'].style || '');
+    img.setAttribute('class', 'vjs-thumbnail');
+
+    // add the thumbnail to the player
+    progressControl = this.controlBar.progressControl.el();
+    progressControl.appendChild(img);
+  });
+})();
